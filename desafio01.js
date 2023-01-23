3fd3d098ffb704210116af0ca39eb94e4e691b31
manuortega12 <manuortega12@gmail.com>
2023-01-23 19:16:21 -0300

    mod

--- desafio01.js
+++ desafio01.js
@@ -41,5 +41,7 @@ class ProductManager {
 const productManager = new ProductManager()
 productManager.addProduct('Teclado', 'Teclado Redragon Shiva K512 RGB Español Black', 8000, 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_24185_Teclado_Redragon_Shiva_K512_RGB_Espa__ol_Black_970bc239-grn.jpg', '205010', 20)
 productManager.addProduct('Mouse', 'Mouse Logitech G PRO Hero Gaming 16000DPI RGB', 9000, 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_9518_Mouse_Logitech_G_PRO_Hero_Gaming_16000DPI_RGB_2934063c-grn.jpg', '306807', 10)
-productManager.addProduct('Gabinete', 'Gabinete Kolink Void Black ARGB ATX Vidrio Templado ', 24000, 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_20250_Gabinete_Kolink_Void_Black_ARGB_ATX_Vidrio_Templado_f962dc11-grn.jpg', '640392', 8)
+productManager.addProduct('Gabinete', 'Gabinete Kolink Void Black ARGB ATX Vidrio Templado ', 24000, 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_20250_Gabinete_Kolink_Void_Black_ARGB_ATX_Vidrio_Templado_f962dc11-grn.jpg', '205010', 8)
+console.log(productManager.getProducts())
+productManager.addProduct('Gabinete', 'Gabinete Kolink Void Black ARGB ATX Vidrio Templado ', 24000, 'https://compragamer.net/pga/imagenes_publicadas/compragamer_Imganen_general_20250_Gabinete_Kolink_Void_Black_ARGB_ATX_Vidrio_Templado_f962dc11-grn.jpg', 8)
 console.log(productManager.getProducts())
